f5f326b0549e4ddbe16dee4b3160b4e21bc3359c
Radhika-2002 <2019radhika.katiyara@ves.ac.in>
2024-06-13 19:01:54 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,18 +1,18 @@
-//your JS code here. If required.
-function updateTime()) {
+function updateTime() {
 	const timerElement = document.getElementById("timer");
 	const now = new Date();
 	const year = now.getFullYear();
-	const month = String(now.getMonth()+1).padStart(2, "0");
+	const month = String(now.getMonth() + 1).padStart(2, "0");
 	const day = String(now.getDate()).padStart(2, "0");
-	const hours = now.getHours();
+	let hours = now.getHours();
 	const minutes = String(now.getMinutes()).padStart(2, "0");
-	const seconds = String(now.getSeconds).padStart(2, "0");
-	const ampm = hours>=12 : "PM" : "AM";
+	const seconds = String(now.getSeconds()).padStart(2, "0");
+	const ampm = hours >= 12 ? "PM" : "AM";
 
-	const formattedHours = hours%12 || 12;
-	const formattedTime = '${month}/${day}/${year},${formattedHours}:${minutes}:${seconds} ${ampm}';
+	hours = hours % 12 || 12; // Convert 24-hour format to 12-hour format
+	const formattedTime = `${month}/${day}/${year}, ${hours}:${minutes}:${seconds} ${ampm}`;
 	timerElement.textContent = formattedTime;
 }
-setInterval(updateTime,1000);
-// updateTime();
+
+setInterval(updateTime, 1000);
+updateTime();
